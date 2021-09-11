3208f95453bfc803cae2b4f2d29e71f8ccadf0b3
hel1yeah <hel1_yeah@ukr.net>
2021-09-11 18:07:17 +0300

    Global Objects

---
+++ global.js
@@ -0,0 +1,10 @@
+// console.log(global);
+
+// setTimeout(() => {
+//   console.log('hello world');
+// }, 2000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+console.log(process.argv);
